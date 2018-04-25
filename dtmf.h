503ad53733f89f8d209c126052941cdb2946f84b
mlbz8 <mlbz8@DESKTOP-759URF6>
2018-04-25 19:51:27 -0400

    #5 with report

--- dtmf.h
+++ dtmf.h
@@ -5,10 +5,6 @@
 #include "display.h"
 #include "maplogic.h"
 
-typedef enum {t1,t2,t3,
-              m1,m2,m3,
-              b1,b2,b3,x} circle_place;
-
 void     DTMFAddSample(unsigned x);
 void     DTMFReset();
 unsigned DTMFPower();
